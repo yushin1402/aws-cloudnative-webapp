fc9e4f10e98d0ab1524f4995af27bd4d61502a72
yushin1402 <s.masaomi16h@gmail.com>
2021-12-02 00:23:24 +0900

    finish dockerfile create

--- backend-for-frontend/src/test/java/com/example/backendforfrontend/config/BackendForFrontendApplicationTests.java
+++ backend-for-frontend/src/test/java/com/example/backendforfrontend/config/BackendForFrontendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.example.backendforfrontend.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendForFrontendApplicationTests {
 
 	@Test
 	void contextLoads() {
--- backend/src/test/java/com/example/backend/config/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/config/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backendforfrontend;
+package com.example.backend.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendForFrontendApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
